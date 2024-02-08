695f491ff325aaabbe8cd4d9cd713debc4934acf
MadhanRaghul <madhanraghul2002@gmail.com>
2024-02-08 19:10:49 +0530

    yeay

--- Assignments/Mouse events/index.js
+++ Assignments/Mouse events/index.js
@@ -1,3 +1,18 @@
 document.getElementById('btn1').onclick = function yeah(){
     document.getElementById('btn1').style.backgroundColor = 'violet'
 }
+document.getElementById('btn2').onmouseover = function yeah(){
+    document.getElementById('btn2').style.backgroundColor = 'violet'
+}
+document.getElementById('btn3').ondblclick = function yeah(){
+    document.getElementById('btn3').style.backgroundImage = 'linear-gradient(rgb(250, 128, 244),rgb(201, 97, 246))'
+}
+document.getElementById('btn4').onmouseout = function yeah(){
+    document.getElementById('btn4').style.backgroundColor = 'violet'
+}
+document.getElementById('ipt1').onfocus = function yeah(){
+    document.getElementById('ipt1').style.backgroundColor = 'violet'
+}
+document.getElementById('ipt2').onblur = function yeah(){
+    document.getElementById('ipt2').style.backgroundColor = 'violet'
+}
